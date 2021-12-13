603da486168c2122e893541b698c37eef53cf24e
Alex Boyne-Aitken <abaitken@live.co.uk>
2021-12-13 17:13:45 +0000

    Documented solid colour pattern

--- Source/Lighting/Patterns/PatternSolidColor.cs
+++ Source/Lighting/Patterns/PatternSolidColor.cs
@@ -2,6 +2,9 @@
 
 namespace Lighting.Patterns
 {
+    /// <summary>
+    /// Pattern which sets all lights to the same colour
+    /// </summary>
     public class PatternSolidColor : Pattern
     {
         public Color Color { get; set; }
