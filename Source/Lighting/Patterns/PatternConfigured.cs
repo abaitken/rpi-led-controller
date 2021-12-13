8eca28deac465a666d7bef6bdbddd8f844b970a8
Alex Boyne-Aitken <abaitken@live.co.uk>
2021-12-13 17:16:11 +0000

    Added a configured pattern

---
+++ Source/Lighting/Patterns/PatternConfigured.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Lighting.Patterns
+{
+    public class PatternConfigured : Pattern
+    {
+        private Color[] _colours;
+
+        public Color[] Configured { get; set; }
+
+        public sealed override Color this[int index] => _colours[index];
+
+        public override sealed void NextState(Random random)
+        {
+        }
+
+        public override sealed void Reset(ILightingInformation information, Random random)
+        {
+            _colours = (from index in Enumerable.Range(0, information.LightCount)
+                        select information[index].Color).ToArray();
+
+            for (int i = 0; i < Math.Min(Configured.Length, information.LightCount); i++)
+                _colours[i] = Configured[i];
+        }
+    }
+}
